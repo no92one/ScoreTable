b74f93231910592bf63db001837b2ddd3ff5d3c9
no92one <linus.lindroth.92@gmail.com>
2023-11-07 12:07:23 +0100

    Delegerad eventhantering med closest-metoden

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const headLine = document.createElement("h1")
-headLine.innerText = "Scoreboard!"
+const headLine = document.createElement("h1");
+headLine.innerText = "Scoreboard!";
 
+// Lista med spelare
 const users = [
   {
     name: "Player1",
@@ -26,110 +27,95 @@ const users = [
     name: "Player6",
     score: 0
   }
-]
+];
 
 // Skapa table element
-const tabel = document.createElement("table")
-const thead = document.createElement("thead")
-const tbody = document.createElement("tbody")
-const trHead = document.createElement("tr")
-const thName = document.createElement("th")
-const thScore = document.createElement("th")
-const thAdd1 = document.createElement("th")
-const thRemove1 = document.createElement("th")
-const thAdd5 = document.createElement("th")
-const thRemove5 = document.createElement("th")
+const table = document.createElement("table");
+const thead = document.createElement("thead");
+const tbody = document.createElement("tbody");
+const trHead = document.createElement("tr");
+const thName = document.createElement("th");
+const thScore = document.createElement("th");
+const thAdd1 = document.createElement("th");
+const thRemove1 = document.createElement("th");
+const thAdd5 = document.createElement("th");
+const thRemove5 = document.createElement("th");
 
 // Stoppa in text i table head columns
-thName.innerText = "Player Name"
-thAdd1.innerText = "Add 1"
-thRemove1.innerText = "Remove 1"
-thAdd5.innerText = "Add 5"
-thRemove5.innerText = "Remove 5"
-thScore.innerText = "Player Score"
+thName.innerText = "Player Name";
+thAdd1.innerText = "Add 1";
+thRemove1.innerText = "Remove 1";
+thAdd5.innerText = "Add 5";
+thRemove5.innerText = "Remove 5";
+thScore.innerText = "Player Score";
 
 // Bygger ihopa min table
-trHead.appendChild(thName)
-trHead.appendChild(thAdd1)
-trHead.appendChild(thRemove1)
-trHead.appendChild(thAdd5)
-trHead.appendChild(thRemove5)
-trHead.appendChild(thScore)
-thead.appendChild(trHead)
-tabel.appendChild(thead)
-tabel.appendChild(tbody)
+trHead.appendChild(thName);
+trHead.appendChild(thAdd1);
+trHead.appendChild(thRemove1);
+trHead.appendChild(thAdd5);
+trHead.appendChild(thRemove5);
+trHead.appendChild(thScore);
+thead.appendChild(trHead);
+table.appendChild(thead);
+table.appendChild(tbody);
 
 let index = 0;
 
 for (let user of users) {
   // Skapa alla table element för varje spelar rad 
-  const playerRow = document.createElement("tr")
-  const playerName = document.createElement("td")
-  const playerAdd1 = document.createElement("td")
-  const playerRemove1 = document.createElement("td")
-  const playerAdd5 = document.createElement("td")
-  const playerRemove5 = document.createElement("td")
-  const playerScore = document.createElement("td")
+  const playerRow = document.createElement("tr");
+  const playerName = document.createElement("td");
+  const playerAdd1 = document.createElement("td");
+  const playerRemove1 = document.createElement("td");
+  const playerAdd5 = document.createElement("td");
+  const playerRemove5 = document.createElement("td");
+  const playerScore = document.createElement("td");
 
-  // lägger till text i varje spelar rads kolumn 
-  playerName.innerText = user.name
-  playerAdd1.innerText = "+ 1"
-  playerRemove1.innerText = "- 1"
-  playerAdd5.innerText = "+ 5"
-  playerRemove5.innerText = "- 5"
-  playerScore.innerText = user.score
+  // Lägger till text i varje spelar rads kolumn 
+  playerName.innerText = user.name;
+  playerAdd1.innerText = "+ 1";
+  playerRemove1.innerText = "- 1";
+  playerAdd5.innerText = "+ 5";
+  playerRemove5.innerText = "- 5";
+  playerScore.innerText = user.score;
 
-  playerAdd1.id = index
-  playerRemove1.id = index
-  playerAdd5.id = index
-  playerRemove5.id = index
-  playerScore.id = index
+  // Lägger till ett id på varje rad. 
+  playerRow.id = index;
 
-  playerAdd1.classList.add("add1")
-  playerRemove1.classList.add("remove1")
-  playerAdd5.classList.add("add5")
-  playerRemove5.classList.add("remove5")
-  playerScore.classList.add("playerScore")
+  // Lägger till en class för varje kolumn
+  playerAdd1.classList.add("add1");
+  playerRemove1.classList.add("remove1");
+  playerAdd5.classList.add("add5");
+  playerRemove5.classList.add("remove5");
+  playerScore.classList.add("playerScore");
 
+  // Stoppar in kolumnerna i raden
+  playerRow.appendChild(playerName);
+  playerRow.appendChild(playerAdd1);
+  playerRow.appendChild(playerRemove1);
+  playerRow.appendChild(playerAdd5);
+  playerRow.appendChild(playerRemove5);
+  playerRow.appendChild(playerScore);
 
-
-
-  // lägg till event för add och remove kolumnen
-  // playerAdd1.addEventListener("click", function () {
-  //   user.score++;
-  //   playerScore.innerText = user.score
-  // })
-
-  // playerRemove1.addEventListener("click", function () {
-  //   user.score--;
-  //   playerScore.innerText = user.score
-  // })
-
-  // playerAdd5.addEventListener("click", function () {
-  //   user.score += 5;
-  //   playerScore.innerText = user.score
-  // })
-
-  // playerRemove5.addEventListener("click", function () {
-  //   user.score -= 5;
-  //   playerScore.innerText = user.score
-  // })
-
-  playerRow.appendChild(playerName)
-  playerRow.appendChild(playerAdd1)
-  playerRow.appendChild(playerRemove1)
-  playerRow.appendChild(playerAdd5)
-  playerRow.appendChild(playerRemove5)
-  playerRow.appendChild(playerScore)
-
-  tbody.appendChild(playerRow)
+  // Stoppar in raden i tbody
+  tbody.appendChild(playerRow);
   index++;
 }
 
+// Lägger till en eventlistener på tbody
 tbody.addEventListener("click", function (event) {
-  const target = event.target;
-  const playerIndex = target.getAttribute("id")
 
+  // Hämtar vilket html-element som användaren klickade på
+  const target = event.target;
+
+  // Använder closest-metoden för att hämtar raden som elementet la i.
+  const playerRow = event.target.closest('tr');
+
+  // Hämtar ut index värde från raden.
+  const playerIndex = playerRow.id;
+
+  // Kollar vilken av poäng förändring kolumnerna som användaren tryckte på
   if (target.matches(".add1")) {
     users[playerIndex].score++;
   } else if (target.matches(".remove1")) {
@@ -140,10 +126,12 @@ tbody.addEventListener("click", function (event) {
     users[playerIndex].score -= 5;
   }
 
-  document.getElementsByClassName("playerScore")[playerIndex].innerText = users[playerIndex].score
+  // Väljer score-kolumnen från raden
+  const playerScore = playerRow.querySelector('.playerScore');
+
+  // Uppdaterar text i score-kolumnen
+  playerScore.innerText = users[playerIndex].score;
 })
 
-document.body.appendChild(headLine)
-document.body.appendChild(table)
-
-
+document.body.appendChild(headLine);
+document.body.appendChild(table);
